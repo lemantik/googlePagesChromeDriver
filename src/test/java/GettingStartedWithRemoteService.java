79558ee2b9ccbf2e972f12b7a54fa9f6ec0925ea
Anton <alemiasheuski@expediagroup.com>
2019-10-29 09:57:32 -0700

    add classes for chromeDriver execution

---
+++ src/test/java/GettingStartedWithRemoteService.java
@@ -0,0 +1,41 @@
+import org.junit.*;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
+import java.io.IOException;
+import java.net.URL;
+
+import static java.lang.Thread.*;
+
+public class GettingStartedWithRemoteService {
+
+    private static WebDriver drv;
+
+    @BeforeClass
+    public static void setRemoteService() throws IOException {
+        ChromeOptions options = new ChromeOptions();
+        DesiredCapabilities cap = new DesiredCapabilities();
+        cap.setCapability("appdriver-options", options);
+        URL url = new URL("http://localhost:9515");
+        drv = new RemoteWebDriver(url, cap);
+    }
+
+    @After
+    public void quitDriver() {
+        drv.quit();
+    }
+
+    @Test
+    public void testGoogleSearch() throws InterruptedException {
+        drv.get("http://www.google.com/");
+        sleep(5000);  // Let the user actually see something!
+        WebElement searchBox = drv.findElement(By.name("q"));
+        searchBox.sendKeys("ChromeDriver");
+        searchBox.submit();
+        sleep(5000);  // Let the user actually see something!
+    }
+}
---
+++ src/test/java/GettingStartedWithService.java
@@ -0,0 +1,56 @@
+import java.io.*;
+import org.junit.*;
+import org.openqa.selenium.*;
+import org.openqa.selenium.chrome.*;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
+import static java.lang.Thread.sleep;
+
+public class GettingStartedWithService {
+
+    private static ChromeDriverService service;
+    private WebDriver driver;
+
+    @BeforeClass
+    public static void createAndStartService() throws IOException {
+        service = new ChromeDriverService.Builder()
+                .usingDriverExecutable(new File("./chromedriver"))
+                .usingAnyFreePort()
+                .build();
+        service.start();
+    }
+
+    @AfterClass
+    public static void stopService() {
+        service.stop();
+    }
+
+    @Before
+    public void createDriver() {
+        ChromeOptions options = new ChromeOptions();
+        DesiredCapabilities cap = new DesiredCapabilities();
+        cap.setCapability("appdriver-options", options);
+        driver = new RemoteWebDriver(service.getUrl(), cap);
+    }
+
+    @After
+    public void quitDriver() {
+        driver.quit();
+    }
+
+    @Test
+    public void testGoogleSearch() throws InterruptedException {
+        driver.get("http://www.google.by");
+        sleep(5000);  // Let the user actually see something!
+        WebElement searchBox = driver.findElement(By.name("q"));
+        searchBox.sendKeys("ChromeDriver");
+        searchBox.submit();
+        sleep(5000);
+    }
+
+    @Test
+    public void testTutBy() {
+        driver.get("http://www.tut.by");
+    }
+}
