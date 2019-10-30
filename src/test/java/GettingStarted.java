ce33980a6a9893f3b9824752bf78b32a84d4e17e
Anton <alemiasheuski@expediagroup.com>
2019-10-29 17:13:47 -0700

    add ChromeOptions

--- src/test/java/GettingStarted.java
+++ src/test/java/GettingStarted.java
@@ -9,7 +9,17 @@ public class GettingStarted {
         // Optional. If not specified, WebDriver searches the PATH for chromedriver.
         System.setProperty("webdriver.chrome.driver", "./chromedriver");
 
-        WebDriver driver = new ChromeDriver();
+        ChromeOptions options = new ChromeOptions();
+
+        options.addArguments("headless");
+        options.addArguments("window-size=1280x800");
+        options.addArguments("no-sandbox");
+        options.addArguments("–disable-dev-shm-usage");
+        options.addArguments("start-maximized");
+        options.addArguments("--disable-gpu");
+        options.addArguments("--disable-setuid-sandbox");
+
+        WebDriver driver = new ChromeDriver(options);
         driver.get("http://www.google.com/");
         Thread.sleep(5000);  // Let the user actually see something!
         WebElement searchBox = driver.findElement(By.name("q"));
